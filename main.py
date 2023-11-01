f184d44a04eba629c5c75381e9701b4b3c6d51ad
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2023-11-01 18:50:54 +0000

    Create sort_files function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 File sorter sorts files in folders named by the file extensions
 '''
 import os
+import shutil
 
 
 def create_folder(path: str, extension: str) -> str:
@@ -15,3 +16,22 @@ def create_folder(path: str, extension: str) -> str:
         os.makedirs(folder_path)
 
     return folder_path
+
+
+def sort_files(source_path: str):
+    '''
+    Sorts the files based on a given path
+    '''
+
+    # go through all the folders and files of the source_path
+    for root_dir, sub_dir, filenames in os.walk(source_path):
+        for filename in filenames:
+            file_path: str = os.path.join(root_dir, filename)
+            # split extension of file or return empty string
+            extension: str = os.path.splitext(filename)[1]
+
+            if extension:
+                target_folder: str = create_folder(source_path, extension)
+                target_path: str = os.path.join(target_folder, filename)
+
+                shutil .move(file_path, target_path)
